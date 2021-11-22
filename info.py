7cedf28886fdd190580628d26b2f2f927a930b67
Pro.K1B3R <79110760+prokiber@users.noreply.github.com>
2021-11-22 17:05:03 +0000

    Create info.py

---
+++ info.py
@@ -0,0 +1,36 @@
+
+ class Lobby(object):
+    def __init__(self):
+        self.game={}
+        
+        
+class Ninja(object):
+    def __init__(self):
+        self.hp=20
+        self.damage=4
+        self.evade=10
+        self.accuracy=10
+        self.cost=80
+        
+        
+class Robot(object):
+    def __init__(self):
+        self.hp=25
+        self.damage=3
+        self.evade=2
+        self.accuracy=10
+        self.cost=80
+        
+        
+class Berserk(object):
+    def __init__(self):
+        self.hp=25
+        self.damage=6
+        self.evade=5
+        self.accuracy=10
+        self.cost=80
+        
+berserk=Berserk()        
+robot=Robot()        
+ninja=Ninja()        
+lobby = Lobby()
